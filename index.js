472e56f327ff519c8fb3c0a588c8fecc59862cb0
leocro-ensea <leonard.croce@ensea.fr>
2024-10-25 09:29:22 +0200

    Debut : Séparation des responsabilités

--- index.js
+++ index.js
@@ -6,5 +6,32 @@ const usersRouter = require("./routes/users.js")
 app.use(express.json())
 app.use("/api/", usersRouter)
 app.listen(port, () => {
-    console.log('Server running at http://localhost/${port}')
+    console.log('Server running at http://localhost:${port}')
 })
+
+const sqlite3 = require("sqlite3").verbose()
+
+// Open the database connection
+const db = new sqlite3.Database("./users.db", (err) => {
+	if (err) {
+		console.error("Error opening database:", err.message)
+	} else {
+		console.log("Connected to the SQLite database.")
+
+		// Create the items table if it doesn't exist
+		db.run(
+			`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        firstName TEXT NOT NULL,
+        lastName TEXT NOT NULL
+      )`,
+			(err) => {
+				if (err) {
+					console.error("Error creating table:", err.message)
+				}
+			}
+		)
+	}
+})
+
+module.exports = db
