6494f43af3dac13c1b27b7c3aa64320cff8c6cc2
leocro-ensea <leonard.croce@ensea.fr>
2024-10-25 12:10:35 +0200

    "Ajout Methode PUT"

--- routes/users.js
+++ routes/users.js
@@ -1,64 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const { getAllUsers } = require("../controllers/usersControllers")
 const db = require("./database")
+const { createNewUser } = require("../controllers/usersControllers")
 
 module.exports = router
 
-router.post("/", (req, res) => {
-	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
-	const { firstName, lastName } = req.body
+router.post("/",createNewUser)
 
-	// récupérer l'ID du dernier utilisateur en fonction du nombre d'utilisateurs dans notre variable de tableau 'users'.
-	const lastId = users[users.length - 1].id
-	// ajouter un pour créer un utilisateur unique
-	const newId = lastId + 1
+router.put("/:id", updateUser)
 
-	// créer le nouvel utilisateur avec les données du corps de la requête et l'ID calculé
-	const newUser = {
-		firstName,
-		lastName,
-		id: newId,
-	}
-
-	// ajouter le nouvel utilisateur à notre liste d'utilisateurs en utilisant la méthode 'push'
-	users.push(newUser)
-	// envoyer le code de statut 201 (créé) et les données du nouvel utilisateur afin de confirmer au client.
-	res.status(201).json(newUser)
-})
-
-router.put("/:id", (req, res) => {
-	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
-	const { firstName, lastName, role} = req.body
-    const id = parseInt(req.params.id)
-
-	// trouve son index, verifier si le userIndex est positive
-	const userIndex = users.findIndex((user) => user.id === id)
-
-	// utilisateur non trouvé
-	if (userIndex < 0)
-		return res.status(404).json({ msg: "utilisateur non trouvé" })
-
-	// si el est trouvé, 
-
-	if (firstName) users[userIndex].firstName = firstName
-	if (lastName) users[userIndex].lastName = lastName
-	if (role) users[userIndex].role = role
-
-    res.json({
-		msg: "utilisateur mis à jour",
-		user: users[userIndex],
-	})
-})
-
-router.get("/:id", (req, res) => {
-    db.all('SELECT * FROM users', [], (err, rows) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-        } else {
-          res.json(rows);
-        }
-      });
-})
+router.get("/users",getAllUsers)
 
 router.delete("/:id", (req, res) => {
 	const id = parseInt(req.params.id)
